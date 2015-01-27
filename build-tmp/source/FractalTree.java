15296850ff2c81f454062afa95c5f045857de298
unknown <vasee@697-334-Rohan.sf.k12.internal>
2015-01-27 10:21:05 -0800

    Created fractal coral

--- build-tmp/source/FractalTree.java
+++ build-tmp/source/FractalTree.java
@@ -1,6 +1,22 @@
-private double fractionLength = .8; 
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FractalTree extends PApplet {
+
+private double fractionLength = .8f; 
 private int smallestBranch = 10; 
-private double branchAngle = .5;  
+private double branchAngle = .5f;  
 
 public void setup() 
 {   
@@ -32,3 +48,12 @@ public void drawBranches(int x,int y, double branchLength, double angle)
 		drawBranches(endX1, endY2, branchLength, angle2);
 	}
 } 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FractalTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
